e724e190c91e8912980369b546c17207a433b5f6
rafalbalcerzak <raffi.balcer@gmail.com>
2017-02-13 13:03:34 +0100

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std; 
+int ip[4]= {123,124,345, 375};
+
+int main(){
+	cout<<ip[0]<<endl;
+	cout<<ip[1]<<endl;
+	cout<<ip[2]<<endl;
+	cout<<ip[3]<<endl;
+	
+}
