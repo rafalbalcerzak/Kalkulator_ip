437bb05cb38874d405871b6a9e17bd1960a38c9f
rafalbalcerzak <raffi.balcer@gmail.com>
2017-02-14 11:33:00 +0100

    Add files via upload

---
+++ symbol.cpp
@@ -0,0 +1,60 @@
+	
+int symbol_nh_fnc(int skrotin){
+	if(skrotin>=8){
+		skrotin=symbol;
+		cout<<"nnnnnnnn";
+		symbol -=8;
+	}
+	if(skrotin==7){
+		skrotin=symbol;
+		cout<<"nnnnnnnh";
+		symbol -=7;
+	}
+	if(skrotin==6){
+		skrotin=symbol;
+		cout<<"nnnnnnhh";
+		symbol -=6;
+	}
+	if(skrotin==5){
+		skrotin=symbol;
+		cout<<"nnnnnhhh";
+		symbol -=5;
+	}
+	if(skrotin==4){
+		skrotin=symbol;
+		cout<<"nnnnhhhh";
+		symbol -=4;
+	}
+	if(skrotin==3){
+		skrotin=symbol;
+		cout<<"nnnhhhhh";
+		symbol -=3;
+	}
+	if(skrotin==2){
+		skrotin=symbol;
+		cout<<"nnhhhhhh";
+		symbol -=2;
+	}
+	if(skrotin==1){
+		skrotin=symbol;
+		cout<<"nhhhhhhh";
+		symbol -=1;
+	}
+	if(skrotin==0){
+		skrotin=symbol;
+		cout<<"hhhhhhhh";
+	}
+	
+}
+	
+	
+	
+	//funkcja do0 zapisu symbolicznego
+	cout<<"Adres IP w formie symbolicznej:"<<endl;
+	symbol_fnc(symbol;
+	cout<<".";
+	symbol_fnc(symbol;
+	cout<<".";
+	symbol_fnc(symbol);
+	cout<<".";
+	symbol_fnc(symbol);
